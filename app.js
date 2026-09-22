5dd6d3fde5832749e4abf9580eb4599fe75ac655
zsd <zhousd888@gmail.com>
2026-09-22 20:42:24 +0800

    feat: 初始化 CI/CD 演示项目

---
+++ app.js
@@ -0,0 +1,13 @@
+const btn = document.getElementById("btn");
+const result = document.getElementById("result");
+
+btn.addEventListener("click", () => {
+  result.textContent = "项目运行正常，准备进入 CI/CD！";
+});
+
+
+
+
+
+
+
